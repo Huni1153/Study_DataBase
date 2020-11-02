8e82ab91e50b3d383b6635e66a8a10816b0017b0
huni <zxca1520@gmail.com>
2020-11-01 20:41:23 -0800

    database_project create

--- Python_code/app.py
+++ Python_code/app.py
@@ -1,30 +1,32 @@
 import os
 from flask import Flask
 from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
-
-#현재 파일이 실행되는 경로
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# db파일이 저장되어있는 경로
-dbfile = os.path.join(basedir,"db.sqlite")
+from models import db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Test(db.Model):
-    __tablename__ = 'test_table'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(32), unique = True)
-
-db.create_all()
+# @app.route('/register')
+# def register():
+#     return render_template('register.html')
 
 @app.route('/')
-
 def hello():
-    return "Hello flask server"
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    print('hello')
+    basedir = os.path.abspath(os.path.dirname(__file__))  #현재 파일이 실행되는 경로
+    print('basedir:{}'.format(basedir))
+    dbfile = os.path.join(basedir,'../DB_code/db.sqlite') # db파일이 저장되어있는 경로
+    print('file:[]'.format(dbfile))
+    # SQLITE Database uri address
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+    # True로 설정하면 각 리퀘스트의 끝에 데이터베이스 변동사항을 자동 커밋한다.
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    # 개체 수정을 추적하고 신호를 방출한다. 일반적으로 잘 사용하지 않고 추가 메모리가 필요하여 False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+    app.run(host='0.0.0.0', port = 5007, debug= True)
--- Python_code/sqlEx3.py
+++ Python_code/sqlEx3.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("../DB_code/chadwick.db")
+conn = sqlite3.connect("../DB_code/db.sqlite")
 
 print( "Opened database successfully")
 
@@ -8,3 +8,6 @@ cursor = conn.execute("select yearID from Salaries limit 10")
 
 for row in cursor:
     print("yearID=", row[0])
+
+
+
